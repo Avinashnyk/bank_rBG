3927f578cc707fc8b7427b8e0d030bc94b95f4ed
Avinashnyk <avinash8694@gmail.com>
2025-07-14 16:49:25 +0530

    adding files

---
+++ flipkart.java
@@ -0,0 +1,8 @@
+public class flipkart
+{
+	public static void main(String[] arg)
+	{
+		System.out.println(" New file creation with code from git");
+	}
+
+
---
+++ flipkart1.java
@@ -0,0 +1,8 @@
+public class flipkart2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("creating a file without touch command");
+	}
+}
+
